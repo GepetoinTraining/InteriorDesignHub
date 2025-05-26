c9cafcd56e319accc6d586d2f137be747b2d3102
GepetoinTraining <garcia.pedro.wow@gmail.com>
2025-05-26 10:42:13 -0300

    env viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,14 @@ export default defineConfig(({ mode }) => {
     return {
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'FIREBASE_API_KEY': JSON.stringify(env.FIREBASE_API_KEY),
+        'FIREBASE_AUTH_DOMAIN': JSON.stringify(env.FIREBASE_AUTH_DOMAIN),
+        'FIREBASE_PROJECT_ID': JSON.stringify(env.FIREBASE_PROJECT_ID),
+        'FIREBASE_STORAGE_BUCKET': JSON.stringify(env.FIREBASE_STORAGE_BUCKET),
+        'FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(env.FIREBASE_MESSAGING_SENDER_ID),
+        'FIREBASE_APP_ID': JSON.stringify(env.FIREBASE_APP_ID),
+        'FIREBASE_MEASUREMENT_ID': JSON.stringify(env.FIREBASE_MEASUREMENT_ID),
       },
       resolve: {
         alias: {
